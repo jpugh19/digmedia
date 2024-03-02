48d9d23f7efeeaf4be24b262fc78f59bacfd74e4
jpugh19 <jpugh19@lsu.edu>
2024-03-02 13:27:56 -0600

    dma6

--- dma6/sketch.js
+++ dma6/sketch.js
@@ -1,19 +1,14 @@
-
-let synth = new Tone.PolySynth(Tone.Synth);
+let synth1 = new Tone.PolySynth(Tone.Synth);
+let synth2 = new Tone.PolySynth(Tone.DuoSynth);
 let bend = new Tone.PitchShift();
+let delay = new Tone.FeedbackDelay("8n", 0.5);
 let notes;
 
 bend.pitch = 0;
-synth.connect(bend);
-bend.toDestination();
-
-
-// function preload() {
-
-// }
-
-
-
+synth1.connect(bend);
+synth2.connect(bend);
+bend.connect(delay);
+delay.toDestination();
 
 function setup() {
   createCanvas(500, 500);
@@ -28,24 +23,46 @@ function setup() {
     'k' : 'C5'
   }
 
+  mySelect = createSelect();
+  mySelect.position(250, 35);
+  mySelect.option('Simple Synth');
+  mySelect.option('Duo Synth');
+
   pitchSlider = createSlider(-12, 12, 0, 0.1);
-  pitchSlider.position (120, 100);
+  pitchSlider.position (175, 150);
   pitchSlider.mouseMoved(()=> bend.pitch = pitchSlider.value());
+
+  delaySlider = createSlider(0, 1, 0.5, 0.05);
+  delaySlider.position(175, 250);
+  delaySlider.mouseMoved(() => delay.delayTime.value = delaySlider.value());
 }
 
-
 function keyPressed() {
-  let playNotes = notes[key];
-  synth.triggerAttack(playNotes);
+  if (mySelect.selected() === 'Simple Synth') {
+    let playNotes = notes[key];
+    synth1.triggerAttack(playNotes);
+  }
+  else if (mySelect.selected() === 'Duo Synth') {
+    let playNotes = notes[key];
+    synth2.triggerAttack(playNotes);
+  }
 }
 
 function keyReleased() {
-  let playNotes = notes[key];
-  synth.triggerRelease(playNotes, '+0.02');
+  if (mySelect.selected() === 'Simple Synth') {
+    let playNotes = notes[key];
+    synth1.triggerRelease(playNotes, '+0.02');
+  }
+  else if (mySelect.selected() === 'Duo Synth') {
+    let playNotes = notes[key];
+    synth2.triggerRelease(playNotes, '+0.02');
+  }
 }
-
 
 function draw() {
   background(220, 22, 56);
-  text ('play a-k for synth', 100, 200);
+  textSize(20);
+  text('Play a-k for: ', 135, 50);
+  text('Move slider for pitch bend', 120, 200);
+  text('Move slider for delay', 150, 300);
 }
--- dma7/sketch.js
+++ dma7/sketch.js
@@ -1,5 +1,26 @@
+
+let sine = new Tone.Synth({
+  oscillator: {
+    type: 'sine'
+  },
+  envolope: {
+    attack: 0.1,
+    decay: 0.1,
+    sustain: 0.1,
+    release: 0.1
+  }
+}).toDestination();
+
+
+
 function setup() {
   createCanvas(400, 400);
+}
+
+function keyPressed() {
+  if (key === 'q') {
+    sine.triggerAttackRelease('c4', 1);
+  }
 }
 
 function draw() {

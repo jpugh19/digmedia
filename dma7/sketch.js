3d4c1d1e820aa46919b69f07f9929eda733af25a
jpugh19 <jpugh19@lsu.edu>
2024-03-19 18:16:35 -0500

    spybomb

--- dma7/sketch.js
+++ dma7/sketch.js
@@ -1,28 +1,46 @@
-
-let sine = new Tone.Synth({
-  oscillator: {
-    type: 'sine'
-  },
-  envolope: {
-    attack: 0.1,
-    decay: 0.1,
-    sustain: 0.1,
-    release: 0.1
-  }
-}).toDestination();
-
-
-
-function setup() {
-  createCanvas(400, 400);
+function preload() {
+  spy = loadImage('assets/Blackspy.png');
+  boom = loadImage('assets/Boom.png');
 }
 
-function keyPressed() {
-  if (key === 'q') {
-    sine.triggerAttackRelease('c4', 1);
-  }
+let timer = 0;
+let fuse = new Tone.Noise("brown");
+let hifilter = new Tone.Filter(10000, "highpass");
+let bomb = new Tone.Noise("white");
+let lofilter =  new Tone.Filter(10000, "lowpass");
+
+fuse.connect(hifilter);
+hifilter.toDestination();
+bomb.connect(lofilter);
+lofilter.toDestination();
+
+function setup() {
+  createCanvas(500, 700);
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed === true) {
+    background(spy);
+    fuse.start();
+    timer += deltaTime / 1000;
+    if (timer > 3) {
+      background(boom);
+      fuse.stop();
+      bomb.start();
+      lofilter.frequency.rampTo(100, 4);
+      if (lofilter.frequency.value === 100) {
+        bomb.stop();
+        lofilter.frequency.value = 1000;
+      }
+    }
+  }
+  else if (mouseIsPressed === false) {
+    background(220, 20, 20);
+    textSize(64);
+    text ('Press mouse.', 50, 100);
+    fuse.stop();
+    bomb.stop();
+    lofilter.frequency.value = 1000;
+    timer = 0;
+  }
 }
